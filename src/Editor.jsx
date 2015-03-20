20b1316c6811c5dd889a17710d673e7163ebec09
Antonio F G Sevilla <antonio@garciasevilla.com>
2015-03-20 21:21:39 +0100

    Reactjs and Sharejs problems live

--- src/Editor.jsx
+++ src/Editor.jsx
@@ -16,22 +16,19 @@ define(["require", "react/react", "ace/ace"], function (require, React) { return
         this.share(editor);
     },
     share: function (editor) {
-        var This = this;
-        require(["sjs/channel/bcsocket","sjs/share/share"], function() {
-            $.ajax({
-                url: "api/sharejs/open/"+This.props.filename,
-                success: function(data) {
-                    require(["sjs/share/ace"], function() {
-                        sharejs.open(data.name, 'text',
-                            location.href.substr(0, location.href.search(/\/[^\/]*$/))+'/api/sharejs/channel',
-                            function(error, doc) {
-                                doc.attach_ace(editor);
-                                editor.getSession().setMode("ace/mode/"+data.mode);
-                            }
-                        );
-                    });
-                }
-            });
+        $.ajax({
+            url: "api/sharejs/open/"+this.props.filename,
+            success: function(data) {
+                require(["sjs/share/ace"], function() {
+                    sharejs.open(data.name, 'text',
+                        location.href.substr(0, location.href.search(/\/[^\/]*$/))+'/api/sharejs/channel',
+                        function(error, doc) {
+                            doc.attach_ace(editor);
+                            editor.getSession().setMode("ace/mode/"+data.mode);
+                        }
+                    );
+                });
+            }
         });
     }
 });
