21480a234ec1a71958712b0b60bd36fc94e6a89d
Antonio F. G. Sevilla <antonio@garciasevilla.com>
2015-05-11 01:55:54 +0200

    Forgot an '#include' :P just like in the old c days

--- src/components/BorjesTree.jsx
+++ src/components/BorjesTree.jsx
@@ -1,6 +1,7 @@
 "use strict";
 
 var React = require('react');
+var types = require('borjes/src/types');
 
 var branch= '<svg xmlns="http://www.w3.org/2000/svg"'
            +' xmlns:xlink="http://www.w3.org/1999/xlink"'
