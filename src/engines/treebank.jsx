02a77c995178e4b8834e4550c7d03f411414f7a6
Antonio F. G. Sevilla <antonio@garciasevilla.com>
2015-05-24 20:54:46 +0200

    Intelligent treebank explodes trees to get nodes (started)

--- src/engines/treebank.jsx
+++ src/engines/treebank.jsx
@@ -2,25 +2,71 @@
 
 var nedb = require('nedb');
 
+var Tree = require('borjes/src/tree');
+var FStruct = require('borjes/src/types').FStruct;
+
 var tbs = {};
 
 self.onmessage = function (e) {
     var m = e.data;
     switch (m.event) {
     case 'store':
-        tbs[m.name].insert(m.trees);
+        for (var i = 0; i<m.trees.length; i++) {
+            store(tbs[m.name], m.trees[i]);
+        }
         break;
     case 'new':
-        tbs[m.name] = new nedb();
+        tbs[m.name] = { db: new nedb(), i: 0 };
         break;
     case 'query':
-        tbs[m.name].find(m.query, function (err, docs) {
+        var q = prepare_query(m.query);
+        var db = tbs[m.name].db;
+        db.find(q, function (err, docs) {
             if (err) {
                 postMessage({ event: 'query', error: err, i: m.i });
             } else {
-                postMessage({ event: 'query', result: docs, i: m.i });
+                var ids = {};
+                for (var j=0; j<docs.length; j++) {
+                    var d = docs[j];
+                    if (ids[d.tree_id]===undefined) {
+                        ids[d.tree_id] = true;
+                    }
+                }
+                db.find({ _id: { $in: Object.keys(ids) } }, function (err, docs) {
+                    if (err) {
+                        postMessage({ event: 'query', error: err, i: m.i });
+                    } else {
+                        postMessage({ event: 'query', result: docs.map(x => x.tree), i: m.i });
+                    }
+                });
             }
         });
         break;
     }
 };
+
+function store (tb, tree) {
+    var id = 'tree_'+(tb.i++);
+    tb.db.insert({ _id: id, tree: tree });
+
+    var sexp = Tree.to_sexp(tree, x => {
+        if (x.borjes === 'fstruct') {
+            return FStruct.get(x, 'symbol').s;
+        } else {
+            return x;
+        }
+    });
+    var queue = [sexp];
+    while (queue.length>0) {
+        var node = queue.shift();
+        setTimeout(tb.db.insert.bind(tb.db, { node, tree_id: id }), 0);
+        for (var i=1; i<node.length; i++) {
+            queue.push(node[i]);
+        }
+    }
+}
+
+function prepare_query (query) {
+    var q = { "node.0": query };
+    return q;
+}
