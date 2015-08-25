bd9d7a6848e179ad7b23d24d0575c898621e284e
Antonio F. G. Sevilla <antonio@garciasevilla.com>
2015-08-25 20:32:44 +0200

    Rule editing incorporates BorjesReact editing

--- src/components/VisualEditor.jsx
+++ src/components/VisualEditor.jsx
@@ -3,7 +3,9 @@
 var React = require('react');
 var Bjs = require('borjes');
 var Tree = Bjs.tree;
+var Rule = Bjs.rule;
 var FStruct = Bjs.types.FStruct;
+var World = Bjs.types.World;
 
 var BorjesReact = require('borjes-react');
 
@@ -14,23 +16,32 @@ class RuleEditor extends React.Component {
     constructor(props) {
         super(props);
         var doc = this.props.doc;
-        var mother, left, right;
-        try {
-            mother = doc.at('mother').get();
-            left = doc.at('left').get();
-            right = doc.at('right').get();
-        } catch (e) { }
-        if (!mother || !left || !right) {
-            mother = mother || FStruct({symbol: 'mother'});
-            left = left || FStruct({symbol: 'left'});
-            right = right || FStruct({symbol: 'right'});
-            doc.at().set({mother, left, right});
-        }
-        this.state = { tree: Tree(mother, [Tree(left), Tree(right)]) };
+        var mother = doc.at('m').get();
+        var daughters = doc.at('d').get();
+        this.state = { tree: Tree(mother, [daughters[0], daughters[1]]), editable: false };
+        doc.on('child op', () => {
+            var mi = doc.at('m').get();
+            var dou = doc.at('d').get();
+            this.setState({ tree: Tree(mi, [dou[0], dou[1]]) });
+        });
+    }
+
+    update (x) {
+        var doc = this.props.doc;
+        var mother = doc.at('m').get();
+        var daughters = doc.at('d').get();
+        doc.at('m').set(x.node);
+        doc.at('d').at(0).set(x.children[0]);
+        doc.at('d').at(1).set(x.children[1]);
+        this.setState({tree: x});
+    }
+
+    editToggle () {
+        this.setState({editable: !this.state.editable});
     }
 
     render () {
-        return <BorjesReact x={this.state.tree} />;
+        return <BorjesReact x={this.state.tree} update={this.update.bind(this)} opts={{editable:this.state.editable}}/>;
     }
 }
 
@@ -55,7 +66,9 @@ class VisualEditor extends React.Component {
 
     add () {
         var doc = this.state.doc;
-        doc.at('rules').push({});
+        var mo = FStruct();
+        World.bind(World(), mo);
+        doc.at('rules').push(Rule(mo, [FStruct(), FStruct()]));
     }
 
     delete (path, i) {
@@ -69,6 +82,11 @@ class VisualEditor extends React.Component {
         this.setState({open: open});
     }
 
+    editToggle (i, e) {
+        this.refs["rule"+i].editToggle();
+        e.stopPropagation();
+    }
+
     render () {
         var rules = this.state.doc.at('rules');
         var lexicon = this.state.doc.at('lexicon').get();
@@ -77,9 +95,10 @@ class VisualEditor extends React.Component {
             <div>
                 {rules.get().map((x, i) => <div key={i} className="tree_row">
                     <div className="tree_header" onClick={this.toggleRow.bind(this, i)}>
+                        <span onClick={this.editToggle.bind(this, i)}>edit</span>
                         <span onClick={this.delete.bind(this, 'rules', i)}>remove</span>
                     </div>
-                    {this.state.open[i]?<RuleEditor doc={rules.at(i)} />:null}
+                    {this.state.open[i]?<RuleEditor ref={"rule"+i} doc={rules.at(i)} />:null}
                 </div>)}
                 <div><button onClick={this.add.bind(this)}>Add</button></div>
             </div>
